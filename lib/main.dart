2dc793921af6dbbfb31244e1d7f9df3d69ab14c9
daskim <ewtk2024@gmail.com>
2024-06-30 15:33:55 +0900

    애ㅁ뮬레이터 실행 전

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a=1;
+  var total=3;//친구 수
   var name=['김영숙','홍길동','피자집'];
+
+  addOne(){
+    setState((){
+      total++;
+  });
+  }
+
+  addName(a){
+    setState(() {
+      name.add(a);
+    });
+  }
 
   @override
   Widget build(BuildContext context){
@@ -22,16 +34,15 @@ class _MyAppState extends State<MyApp> {
       floatingActionButton: FloatingActionButton(
         onPressed: (){
           showDialog(context: context, builder: (context) {
-            return DialogUI(
-            );
+            return DialogUI(addOne:addOne, addName: addName);
           }
           );
         },
       ),
-          appBar:AppBar(backgroundColor: Colors.blue),
+          appBar:AppBar(title:Text(total.toString()),backgroundColor: Colors.blue),
           body:
             ListView.builder(
-              itemCount:3,
+              itemCount:name.length,
               itemBuilder:(c,i){
                 return ListTile(
                   leading: Icon(Icons.account_circle),
@@ -56,7 +67,10 @@ class _MyAppState extends State<MyApp> {
 
 
 class DialogUI extends StatelessWidget {
-  const DialogUI({super.key});
+  DialogUI({Key?key,this.addOne,this.addName}):super(key: key);
+  final addOne;
+  final addName;
+  var inputData=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -66,8 +80,12 @@ class DialogUI extends StatelessWidget {
           height: 300,
           child: Column(
               children: [
-                TextField(),
-                TextButton(onPressed: () {}, child: Text('완료')),
+                TextField(controller: inputData),
+                TextButton(onPressed: () {
+                  addOne();
+                  addName(inputData.text);
+                  Navigator.pop(context);
+                }, child: Text('완료')),
                 TextButton(onPressed: () {
                   Navigator.pop(context);
                 }, child: Text('취소'))
